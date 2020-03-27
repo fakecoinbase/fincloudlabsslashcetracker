4847c156d0b600317d5cd4d6177bbd8f3bf95329
Artiom Baloian <artiom.baloian@gmail.com>
2020-03-27 04:46:57 +0000

    Modified Coinbase Pro tracking functionality as the old library I used was already deprecated.

--- exchanges/coinbase/coinbase_pro.mjs
+++ exchanges/coinbase/coinbase_pro.mjs
@@ -11,72 +11,72 @@
  *   and trades.
  */
 
-import async from 'async';
-import coinbase_pro from 'coinbase-pro';
+import coinbase_pro_node from 'coinbase-pro-node';
 
 import coinbase_data from './market.mjs';
 import { UpdateExchangeDataOnDB } from '../../db/update_db.mjs';
-import { getSupportedCoins } from '../../utils/supported_coins.mjs';
 import {
   getUTCISOFormat,
   HasKey,
   Debug,
-  Sleep
 } from '../../utils/utils.mjs';
 
-// Global variable to store product stats.
-const product_stats = {};
+const {CoinbasePro, WebSocketChannelName, WebSocketEvent} = coinbase_pro_node;
 
 
-class CoinbasePro {
+class CoinbaseProClass {
   // Data members:
   // - db: MongoDB database.
-  // - request_cycle_interval: Request product stats interval in milliseconds.
   // - per_request_interval: Per request interval in milliseconds.
   // - public_client: Public client object of the Coinbase Pro package.
   constructor(db) {
     this.db = db;
-    this.request_cycle_interval = 5 * 60000;
-    this.per_request_interval = 1300;
-    this.public_client = new coinbase_pro.PublicClient();
+    this.per_request_interval = 5000;
+    this.client = new CoinbasePro({
+      apiKey: '',
+      apiSecret: '',
+      passphrase: '',
+      useSandbox: false,
+    });
+    this.channel = {
+      name: WebSocketChannelName.TICKER,
+      product_ids: coinbase_data.product_id,
+    };
   }
-
 
   run() {
-    this.TrackProductStats();
     this.ListenWebsocket();
   }
 
+  async init() {
+    await this.client.ws.connect();
+    this.client.ws.subscribe([this.channel]);
+  }
 
-  ReconnectSocket() {
+  async ReconnectSocket() {
     this.ListenWebsocket();
   }
 
+  async ListenWebsocket() {
+    await this.init();
 
-  // Function listens Coinbase Pro websocket feed, tracks corresponding data,
-  // updates coins current state and stores/updates in the MongoDB database.
-  ListenWebsocket() {
-    const ws = new coinbase_pro.WebsocketClient(coinbase_data.product_id);
-
-    ws.on('message', (ws_data) => {
-      ws_data = CoinbasePro.VerifyWebsocketData(ws_data);
+    this.client.ws.on(WebSocketEvent.ON_MESSAGE_TICKER, (ws_data) => {
+      ws_data = CoinbaseProClass.VerifyWebsocketData(ws_data);
       if (ws_data) {
         UpdateExchangeDataOnDB(this.db, coinbase_data.exchange_name, [ws_data]);
       }
     });
 
-    ws.on('error', (error) => {
+    this.client.ws.on(WebSocketEvent.ON_ERROR, (error) => {
       Debug('Coinbase Pro websocket error:', error);
     });
 
-    ws.on('close', () => {
+    this.client.ws.on(WebSocketEvent.ON_CLOSE, () => {
       const reconnect_interval_ms = 10000;
-      const msg = 'Coinbase Pro websocket was closed, it will reconnect again';
-      Debug(msg);
+      Debug('Coinbase Pro websocket was closed, it will reconnect again');
       setTimeout(() => { this.ReconnectSocket(); }, reconnect_interval_ms);
     });
   }
-
 
   // Function verifies websocket data received from Coinbase Pro websocket.
   // It checks if received data contains corresponding properties and they
@@ -90,33 +90,22 @@ class CoinbasePro {
     // Check if expected keys/properties are provided. I check only properties,
     // which are used in the project.
     if (ws_data && HasKey(ws_data, 'type') &&
-        ws_data.type === 'match' &&
+        ws_data.type === 'ticker' &&
         HasKey(ws_data, 'product_id') &&
         HasKey(ws_data, 'price') &&
+        HasKey(ws_data, 'open_24h') &&
+        HasKey(ws_data, 'volume_24h') &&
         ws_data.product_id && ws_data.price) {
       const product_id = String(ws_data.product_id);
       const ticker = String((product_id.split('-'))[0]);
       const market = String((product_id.split('-'))[1]);
 
-      const supported_coins = getSupportedCoins(coinbase_data.exchange_name);
-      if (!supported_coins || !HasKey(supported_coins, ticker)) {
-        return null;
-      }
-
-      let open_price = null;
-      let volume24h = null;
-      if (product_stats && HasKey(product_stats, ticker) &&
-          HasKey(product_stats[ticker], market)) {
-        open_price = product_stats[ticker][market].open_price;
-        volume24h = product_stats[ticker][market].volume24h;
-      }
-
       const coin_data = {
         ticker: String(ticker),
         market: String(market),
         price: Number(ws_data.price),
-        open_price,
-        volume24h,
+        open_price: Number(ws_data.open_24h),
+        volume24h: Number(ws_data.volume_24h),
         last_update: getUTCISOFormat()
       };
 
@@ -124,54 +113,7 @@ class CoinbasePro {
     }
     return null;
   }
-
-
-  // Note that there is a rate limits. Coinbase throttles public endpoints by IP:
-  // 3 requests per second, up to 6 requests per second in bursts.
-  async getCoinbaseProductStats() {
-    async.eachSeries(coinbase_data.product_id, (market_id, next) => {
-      setTimeout(() => this.public_client.getProduct24HrStats(market_id,
-        (error, response, data) => {
-          if (error) {
-            Debug(error);
-          } else if (data && HasKey(data, 'open') && HasKey(data, 'volume') &&
-                     data.open && data.volume) {
-            const ticker = String((market_id.split('-'))[0]);
-            const market = String((market_id.split('-'))[1]);
-            const open_price = Number(data.open);
-            const volume24h = Math.round(Number(data.volume));
-            if (!HasKey(product_stats, ticker)) {
-              product_stats[ticker] = {};
-            }
-
-            product_stats[ticker][market] = {
-              open_price,
-              volume24h
-            };
-          }
-
-          next();
-        }), this.per_request_interval);
-    }, () => {});
-  }
-
-
-  async TrackProductStats() {
-    while (1) {
-      try {
-        await this.getCoinbaseProductStats();
-      } catch (e) {
-        Debug(e);
-      } finally {
-        if (!this.public_client) {
-          this.public_client = new coinbase_pro.PublicClient();
-        }
-      }
-
-      await Sleep(this.request_cycle_interval);
-    }
-  }
 }
 
-export default CoinbasePro;
+export default CoinbaseProClass;
 
